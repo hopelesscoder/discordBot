186bfca70fb28dddb87c3417da358d1c0c0e72cf
Daniele Pasquini <daniele.pasquini@sistinf.it>
2020-08-06 17:56:54 +0200

    first try

--- bot.js
+++ bot.js
@@ -1,25 +1,32 @@
 const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-const client = new Discord.Client();
-
- 
-
-client.on('ready', () => {
+bot.on('ready', () => {
 
     console.log('I am ready!');
 
 });
 
- 
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
 
-client.on('message', message => {
+bot.on('message', msg => {
+  if (msg.content === '!ping') {
+    msg.reply('pong');
+    msg.channel.send('pong');
 
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+  } else if (msg.content.startsWith('!kick')) {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  } else if (msg.content === '!cit') {
+	  msg.reply("Ma chi è chel mona ch-che-che batte la porta e che chiude u-urlando??!");
+	  msg.channel.send("Ma chi è chel mona ch-che-che batte la porta e che chiude u-urlando??!");
+  }
 });
 
  
